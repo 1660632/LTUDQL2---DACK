c3518b67b64387c35a718485b30637a4adf2b427
Firstname Lastname <ttqtrantg98@gmail.com>
2019-05-01 13:02:16 +0700

    1660632

---
+++ LTUDQL2/DA_LTUDQL2/ViewModel/LoginViewModel.cs
@@ -0,0 +1,101 @@
+﻿using DA_LTUDQL2.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace DA_LTUDQL2.ViewModel
+{
+    public class LoginViewModel:BaseViewModel
+    {
+        public bool IsLogin { get; set; }
+        private string _UserName;
+        private string _Password;
+
+
+        public ICommand LoginCommand { get; set; }
+        public ICommand PasswordChangedCommand { get; set; }
+
+        public string UserName
+        {
+            get
+            {
+                return _UserName;
+            }
+
+            set
+            {
+                _UserName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return _Password;
+            }
+
+            set
+            {
+                _Password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public LoginViewModel()
+        {
+            IsLogin = false;
+            Password = "";
+            UserName = "";
+            LoginCommand = new RelayCommand<Window>((p) => { return true; }, (p) => { { Login(p); } });
+            PasswordChangedCommand = new RelayCommand<PasswordBox>((p) => { return true; }, (p) => { Password = p.Password; });
+        }
+
+        void Login(Window p)
+        {
+            if (p == null)
+                return;
+
+            string passEncode = MD5Hash(Base64Encode(Password));
+            var accCount = DataProvider.Ins.DB.Users.Where(x => x.UserName == UserName && x.Password == passEncode).Count();
+            if (accCount > 0)
+            {
+                IsLogin = true;
+                p.Close();
+            }
+            else
+            {
+                IsLogin = false;
+                MessageBox.Show("Sai tài khoản hoặc mật khẩu!");
+            }
+
+        }
+
+
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string MD5Hash(string input)
+        {
+            StringBuilder hash = new StringBuilder();
+            MD5CryptoServiceProvider md5provider = new MD5CryptoServiceProvider();
+            byte[] bytes = md5provider.ComputeHash(new UTF8Encoding().GetBytes(input));
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hash.Append(bytes[i].ToString("x2"));
+            }
+            return hash.ToString();
+        }
+    }
+}
