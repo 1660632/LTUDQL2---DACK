74feeab9620e19daf3bcf1e06d3bc56f6a6fc909
1660632 <hongtrung21096@gmail.com>
2019-05-13 16:58:43 +0700

    Add 1660661
    Trang chu

---
+++ 1660661/DA_LTUDQL2/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using DA_LTUDQL2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DA_LTUDQL2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<VideoInfo> lst = new List<VideoInfo>
+            {
+                new VideoInfo
+                {
+                },
+                new VideoInfo
+                {
+                },
+                new VideoInfo
+                {
+                },
+                new VideoInfo
+                {
+                },
+                new VideoInfo
+                {
+                },
+                new VideoInfo
+                {
+                },
+                new VideoInfo
+                {
+                },
+                new VideoInfo
+                {
+                },
+                new VideoInfo
+                {
+                },
+                new VideoInfo
+                {
+                },
+                new VideoInfo
+                {
+                },
+                new VideoInfo
+                {
+                },
+                new VideoInfo
+                {
+                },
+            };
+            l1.SetListvideo(lst);
+            l1.SetName("Phim Hành Động");
+            l2.SetListvideo(lst);
+            l2.SetName("Phim Ma");
+            l3.SetListvideo(lst);
+            l3.SetName("Phim Hoạt Hình");
+            l4.SetListvideo(lst);
+            l4.SetName("Phim Viễn Tưởng");
+            l5.SetListvideo(lst);
+            l5.SetName("Phim Cổ Trang");
+            l6.SetListvideo(lst);
+            l6.SetName("Phim Tào Lao");
+        }
+    }
+}
---
+++ 1660661/DA_LTUDQL2/Views/ListVideos.xaml.cs
@@ -0,0 +1,44 @@
+﻿using DA_LTUDQL2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DA_LTUDQL2.Views
+{
+    /// <summary>
+    /// Interaction logic for ListVideos.xaml
+    /// </summary>
+    public partial class ListVideos : UserControl
+    {
+        List<VideoInfo> listvideo;
+        string lName = "";
+        public ListVideos()
+        {
+            InitializeComponent();
+            listvideo = new List<VideoInfo>();
+            lbnamelist.Content = lName;
+            DataContext = listvideo; 
+        }
+        public void SetListvideo(List<VideoInfo> lst)
+        {
+            listvideo.AddRange(lst);
+        }
+        public void SetName(string name)
+        {
+            lName = name;
+            lbnamelist.Content = lName;
+        }
+
+    }
+}
